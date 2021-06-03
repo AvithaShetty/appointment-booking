24cf62b879825e788fd3b185e06eeb58a85b7bfb
Fabhi <43674998+Fabhi@users.noreply.github.com>
2021-06-04 00:44:46 +0530

    Added

--- abhishek/static/appointment.js
+++ abhishek/static/appointment.js
@@ -1,4 +1,7 @@
 var department, doctor, date, time;
+var pages = ["department","doctor","slot", "confirmation", "payment"]
+var currentIndex = 0;
+var loaded = false;
 
 function getContent(URL, target, template, callback, index){
     function display(elem) {
@@ -8,7 +11,7 @@ function getContent(URL, target, template, callback, index){
         template_html = $(template).html()
         data = Mustache.to_html(template_html, elem)
         $(target).append(data);
-        $("#"+elem.id_name).click(()=>{
+        $("#"+elem.id_name).click( ()=>{
             console.log(selectedItemforID);
             callback(selectedItemforID);
         });
@@ -17,7 +20,7 @@ function getContent(URL, target, template, callback, index){
     function f(data) {
         console.log(data)
         data.results.forEach(display);
-    }    
+    }  
     $.get(URL, f);
 }
 
@@ -28,27 +31,56 @@ function getAddr(endpoint){
     return ADDR
 }
 
-function changePage(curr, next){
-    var current_step = "#"+curr+"_step"
-    var next_step = "#"+next+"_step"
-    var current_container = "#"+curr+"_container"
-    var next_container = "#"+next+"_container"
-    $(current_step).addClass("completed");
-    $(current_step).removeClass("active");
-    $(current_container).css('display','none');
+function changePage(nextIndex){
+    console.log("CHANGING PAGE")
+    var currentPage = pages[currentIndex]
+    var nextPage = pages[nextIndex]
+    var current_step = "#"+ currentPage +"_step"
+    var next_step = "#"+nextPage+"_step"
+    var current_container = "#"+currentPage+"_container"
+    var next_container = "#"+nextPage+"_container";
 
-    $(next_step).addClass("active");
-    $(next_step).removeClass("disabled");
-    $(next_container).css('display','block');
+    if(nextIndex < currentIndex){
+        for (var i=nextIndex; i<=currentIndex;i++){
+            var page = pages[i];
+            var page_step = "#"+ page +"_step"
+            var page_container = "#"+page+"_container"
+            $(page_step).removeClass();
+            $(page_step).addClass("link disabled step");
+            $(page_container).css('display','none');
+
+        }
+        $(next_step).addClass("active");
+        $(next_step).removeClass("disabled");
+        $(next_container).css('display','block');
+    }
+    else if(nextIndex > currentIndex){
+        $(current_step).addClass("completed");
+        $(current_step).removeClass("active");
+        $(current_container).css('display','none');
+    
+        $(next_step).addClass("active");
+        $(next_step).removeClass("disabled");
+        $(next_container).css('display','block');
+    }
+    currentIndex = nextIndex;
 }
 
 
 // onClick functions
 function getDoctors(dept_name){
     department = dept_name;
-    getContent(getAddr("/get_doctors/"+dept_name), "#doctor_target", "#doctor_template", () => {},2 )
-    changePage("department","doctor")
+    if (!loaded){
+        getContent(getAddr("/get_doctors/"+dept_name), "#doctor_target", "#doctor_template", getSlot, 2)
+        loaded = true
+    }
+    changePage(1);
 }
 
+function getSlot(doct_name){
+    doctor = doct_name;
+    changePage(2);
+}
 
 getContent(getAddr("/get_departments"), "#department_target", "#department_template", getDoctors, 0);
+
